44172bb7dcc723bdb8b9df9f9c436c4dcae74c9b
Graham Keenan <graham.keenan@outlook.com>
2024-02-20 11:37:25 +0000

    feat(cli): got the skeletal cli defined

--- src/cli.rs
+++ src/cli.rs
@@ -9,18 +9,21 @@ pub struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    /// Upload a file to Waifu Vault
+    /// Upload a file to vault
     Upload(UploadArgs),
 
-    /// Retrieve a file from Waifu Vault
+    /// Retrieve a file from the vault
     Download(DownloadArgs),
 
-    /// Delete a file from Waifu Vault
+    /// Information about a file in the vault
+    Info(InfoArgs),
+
+    /// Delete a file from the vault
     Delete(DeleteArgs),
 }
 
 #[derive(Args, Debug)]
-struct UploadArgs {
+pub struct UploadArgs {
     /// File to upload to the Vault (Max size 100MB)
     #[arg(short, long, conflicts_with = "url", required_unless_present = "url")]
     file: Option<String>,
@@ -43,16 +46,34 @@ struct UploadArgs {
 }
 
 #[derive(Args, Debug)]
-struct DownloadArgs {
+pub struct DownloadArgs {
+    /// Download token generated when the file was uploaded
     #[arg(short, long)]
     token: String,
 
+    /// Where to store the output file
     #[arg(short, long, default_value_t = String::from("."))]
     output: String,
+
+    /// Password if required to download the file
+    #[arg(short, long)]
+    password: Option<String>,
 }
 
 #[derive(Args, Debug)]
-struct DeleteArgs {
+pub struct InfoArgs {
+    /// Token generated from when the file was uploaded
+    #[arg(short, long)]
+    token: String,
+
+    /// Show human-readable timestamp for the file expiry
+    #[arg(short, long)]
+    formatted: bool,
+}
+
+#[derive(Args, Debug)]
+pub struct DeleteArgs {
+    /// Token generated from when the file was uploaded
     #[arg(short, long)]
     token: String,
 }
