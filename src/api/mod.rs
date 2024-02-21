0a3a9308a1562927b158afa2a46a3b3514664bf9
Graham Keenan <graham.keenan01@bbc.co.uk>
2024-02-21 13:28:56 +0000

    refactor: cleaned up the api code a bit

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -11,178 +11,209 @@ use reqwest::{
 
 const API: &'static str = "https://waifuvault.moe/rest";
 
-pub fn upload(args: &UploadArgs) -> Result<()> {
-    let client = Client::new();
-
-    let request = {
-        let mut r = client
-            .put(API)
-            .query(&[("hide_filename", &args.hide_filename)]);
-
-        if let Some(file) = &args.file {
-            let form = Form::new().file("file", file);
-            r = r.multipart(form.unwrap());
-        } else {
-            let url = &args.url.clone().unwrap();
-            r = r.form(&[("url", url)]);
-        }
-
-        if let Some(pwd) = &args.password {
-            r = r.query(&[("password", pwd)]);
-        }
-
-        if let Some(expires) = &args.expires {
-            r = r.query(&[("expires", expires)]);
-        }
-
-        r
-    };
-
-    let response = request.send()?;
-    let status = response.status();
-    let response: ApiResponse = response.json()?;
-
-    parse_response(response, status);
-    Ok(())
+pub struct ApiCaller {
+    client: Client,
+    info_str: String,
 }
 
-pub fn download(args: &DownloadArgs) -> Result<()> {
-    let api_url = format!("{API}/{}", args.token.clone());
-    let response: ApiResponse = Client::new()
-        .get(api_url)
-        .query(&[("formatted", true)])
-        .send()?
-        .json()?;
-    let parsed_response = parse_download_response(response);
-    if parsed_response.is_none() {
-        return Ok(());
+impl ApiCaller {
+    pub fn new() -> Self {
+        Self {
+            client: Client::new(),
+            info_str: String::new(),
+        }
     }
 
-    let output_location = &args.output;
-    let (url, protected) = parsed_response.unwrap();
-    let request = {
-        let mut r = Client::new().get(url);
-        if protected && args.password.is_none() {
-            println!("--= Waifu Vault Client =--\n");
-            println!("This file is password protected and requires a password!");
-            println!();
+    pub fn upload(&mut self, args: &UploadArgs) -> Result<()> {
+        let request = {
+            let mut r = self
+                .client
+                .put(API)
+                .query(&[("hide_filename", &args.hide_filename)]);
+
+            if let Some(file) = &args.file {
+                let form = Form::new().file("file", file);
+                r = r.multipart(form.unwrap());
+            } else {
+                let url = &args.url.clone().unwrap();
+                r = r.form(&[("url", url)]);
+            }
+
+            if let Some(pwd) = &args.password {
+                r = r.query(&[("password", pwd)]);
+            }
+
+            if let Some(expires) = &args.expires {
+                r = r.query(&[("expires", expires)]);
+            }
+
+            r
+        };
+
+        let response = request.send()?;
+        let status = response.status();
+        let response: ApiResponse = response.json()?;
+
+        self.parse_response(response, status);
+        self.display();
+
+        Ok(())
+    }
+
+    pub fn download(&mut self, args: &DownloadArgs) -> Result<()> {
+        let api_url = format!("{API}/{}", args.token.clone());
+        let response: ApiResponse = self
+            .client
+            .get(api_url)
+            .query(&[("formatted", true)])
+            .send()?
+            .json()?;
+
+        let parsed_response = self.parse_download_response(response);
+
+        if parsed_response.is_none() {
+            self.display();
+
             return Ok(());
         }
 
-        match &args.password {
-            Some(pwd) => {
-                r = r.header("x-password", pwd);
+        let output_location = &args.output;
+        let (url, protected) = parsed_response.unwrap();
+        let request = {
+            let mut r = self.client.get(url);
+            if protected && args.password.is_none() {
+                self.add_info("This file is password protected and requires a password!");
+                self.display();
+
+                return Ok(());
             }
-            None => {}
+
+            match &args.password {
+                Some(pwd) => {
+                    r = r.header("x-password", pwd);
+                }
+                None => {}
+            }
+
+            r
+        };
+
+        let response = request.send()?;
+        if response.status() != StatusCode::OK {
+            eprintln!("Error occured!");
+            dbg!(&response);
+            return Ok(());
         }
 
-        r
-    };
+        // TODO: Save file appropriately
+        // Also revisit the output save location
 
-    let response = request.send()?;
-    if response.status() != StatusCode::OK {
-        eprintln!("Error occured!");
-        dbg!(&response);
-        return Ok(());
+        self.add_info("File downloaded successfully and stored at {output_location}!");
+        self.display();
+
+        Ok(())
     }
 
-    // TODO: Save file appropriately
-    // Also revisit the output save location
+    pub fn info(&mut self, args: &InfoArgs) -> Result<()> {
+        let api_url = format!("{API}/{}", args.token.clone());
+        let request = {
+            let mut r = self.client.get(api_url);
+            r = r.query(&[("formatted", true)]);
 
-    println!("--= Waifu Vault Client =--\n");
-    println!("File downloaded successfully and stored at {output_location}!");
-    println!();
-    Ok(())
-}
+            r
+        };
 
-pub fn info(args: &InfoArgs) -> Result<()> {
-    let api_url = format!("{API}/{}", args.token.clone());
-    let request = {
-        let mut r = Client::new().get(api_url);
-        r = r.query(&[("formatted", true)]);
+        let response = request.send()?;
+        let status = response.status();
+        let response: ApiResponse = response.json()?;
+        self.parse_response(response, status);
+        self.display();
 
-        r
-    };
+        Ok(())
+    }
 
-    let response = request.send()?;
-    let status = response.status();
-    let response: ApiResponse = response.json()?;
-    parse_response(response, status);
+    pub fn delete(&mut self, args: &DeleteArgs) -> Result<()> {
+        let api_url = format!("{API}/{}", args.token.clone());
+        let request = self.client.delete(api_url);
+        let response = request.send()?;
+        let status_code = response.status();
+        let response = response.json()?;
 
-    Ok(())
-}
-pub fn delete(args: &DeleteArgs) -> Result<()> {
-    let api_url = format!("{API}/{}", args.token.clone());
-    let request = Client::new().delete(api_url);
-    let response = request.send()?;
-    let status_code = response.status();
-    let response = response.json()?;
+        self.parse_response(response, status_code);
+        self.display();
 
-    parse_response(response, status_code);
+        Ok(())
+    }
 
-    Ok(())
-}
+    fn display(&self) {
+        println!("--= Waifu Vault Client =--\n");
+        println!("{}", self.info_str);
+    }
 
-fn parse_response(response: ApiResponse, status_code: StatusCode) {
-    println!("--= Waifu Vault Client =--\n");
-    match response {
-        ApiResponse::OkResponse {
-            token,
-            url,
-            protected,
-            retention_period,
-        } => {
-            match status_code {
-                StatusCode::OK => println!("File status: File exists!"),
-                StatusCode::CREATED => println!("File status: File stored successfully!"),
-                _ => unreachable!("it's either a 200 or 201"),
+    fn add_info(&mut self, msg: impl AsRef<str>) {
+        self.info_str.push_str(msg.as_ref());
+        self.info_str.push('\n');
+    }
+
+    fn parse_response(&mut self, response: ApiResponse, status_code: StatusCode) {
+        match response {
+            ApiResponse::OkResponse {
+                token,
+                url,
+                protected,
+                retention_period,
+            } => {
+                match status_code {
+                    StatusCode::OK => self.add_info("File exists!"),
+                    StatusCode::CREATED => self.add_info("File stored successfully!"),
+                    _ => unreachable!("it's either a 200 or 201"),
+                }
+
+                self.add_info(format!("It is stored at {url}"));
+                self.add_info(format!("It has the unique token: {token}"));
+                if protected {
+                    self.add_info("It is a PROTECTED file");
+                } else {
+                    self.add_info("It is an UNPROTECTED file");
+                }
+                self.add_info(format!("It is available for {retention_period}"));
             }
-
-            println!("It is stored at {url}");
-            println!("It has the unique token {token}");
-            if protected {
-                println!("It is a PROTECTED file");
-            } else {
-                println!("It is an UNPROTECTED file");
+            ApiResponse::BadResponse {
+                name,
+                message,
+                status: _,
+            } => {
+                self.add_info(format!("Received a bad response from API: {name}"));
+                self.add_info(format!("This is probably due to {message}"));
             }
-            println!("It is available for {retention_period}");
-        }
-        ApiResponse::BadResponse {
-            name,
-            message,
-            status: _,
-        } => {
-            println!("Received a bad response from API: {name}");
-            println!("This is probably due to {message}");
-        }
-        ApiResponse::Delete(result) => {
-            if result {
-                println!("File deleted successfully!");
-            } else {
-                println!("File was NOT deleted successfully...");
+            ApiResponse::Delete(result) => {
+                if result {
+                    self.add_info("File deleted successfully!");
+                } else {
+                    self.add_info("File was NOT deleted successfully...");
+                }
             }
         }
     }
-    println!();
-}
 
-fn parse_download_response(response: ApiResponse) -> Option<(String, bool)> {
-    match response {
-        ApiResponse::OkResponse {
-            token: _,
-            url,
-            protected,
-            retention_period: _,
-        } => Some((url, protected)),
-        ApiResponse::BadResponse {
-            name,
-            message,
-            status: _,
-        } => {
-            println!("Received a bad response from API: {name}");
-            println!("This is probably due to {message}");
-            return None;
+    fn parse_download_response(&mut self, response: ApiResponse) -> Option<(String, bool)> {
+        match response {
+            ApiResponse::OkResponse {
+                token: _,
+                url,
+                protected,
+                retention_period: _,
+            } => Some((url, protected)),
+            ApiResponse::BadResponse {
+                name,
+                message,
+                status: _,
+            } => {
+                self.add_info(format!("Received a bad response from API: {name}"));
+                self.add_info(format!("This is probably due to {message}"));
+                return None;
+            }
+            _ => None,
         }
-        _ => None,
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,13 @@ use cli::*;
 
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
+    let mut api_caller = api::ApiCaller::new();
 
     match &cli.commands {
-        Commands::Upload(args) => api::upload(&args)?,
-        Commands::Info(args) => api::info(&args)?,
-        Commands::Delete(args) => api::delete(&args)?,
-        Commands::Download(args) => api::download(&args)?,
-        _ => {}
+        Commands::Upload(args) => api_caller.upload(&args)?,
+        Commands::Info(args) => api_caller.info(&args)?,
+        Commands::Delete(args) => api_caller.delete(&args)?,
+        Commands::Download(args) => api_caller.download(&args)?,
     }
 
     Ok(())
